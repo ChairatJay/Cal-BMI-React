81b356d796242adc12abf22a1c646bf8ad5b72ce
ChairatJay <147658388+ChairatJay@users.noreply.github.com>
2024-04-22 16:43:07 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import "./index.css"
+
+import Bmi from './components/Bmi'
+
+function App() {
+  
+
+  return (
+    <>
+     <Bmi/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Bmi.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "react";
+
+const Bmi = () => {
+  const [height, setHeight] = useState<number>(0);
+  const [weight, setWeight] = useState<number>(0);
+  const [heading, setHeading] = useState<string>("");
+
+  const onChangeHeight = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setHeight(+event.target.value);
+    
+  };
+
+  const onChangeWeight = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setWeight(+event.target.value);
+    
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const heightMetter = height / 100;
+    
+    const bmi = (weight / (heightMetter * heightMetter)).toFixed(2);
+    console.log(bmi)
+    setHeading(bmi)
+  };
+
+  
+
+  return (
+    <>
+      <h1>Your bmi is {heading}</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="height"> Height:</label>
+        <input type="text" id="username" onChange={onChangeWeight} />
+        <label htmlFor="Weight"> Weight:</label>
+        <input type="text" id="password" onChange={onChangeHeight} />
+        <input type="submit" />
+      </form>
+    </>
+  );
+};
+
+export default Bmi;
